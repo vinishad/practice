2d803d5c715d28bf28eac4d9b53e0a988340b9ec
Vinisha Donthula <vinisha.donthula96@gmail.com>
2018-06-26 21:10:14 +0530

    done till fizz

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,10 @@
+public class FizzBuzz {
+    public String checkFizzBuzz(int input) {
+        if(input % 3 == 0)
+        {
+            return "Fizz";
+        }
+        return String.valueOf(input);
+
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    public void returnoneforone(){
+        String result = new FizzBuzz().checkFizzBuzz(1);
+        assertEquals("1",result);
+    }
+    @Test
+    public void return2for2(){
+        String result = new FizzBuzz().checkFizzBuzz(2);
+        assertEquals("2",result);
+    }
+    @Test
+    public void returnFizzfor3(){
+        String result = new FizzBuzz().checkFizzBuzz(3);
+        assertEquals("Fizz",result);
+    }
+}
